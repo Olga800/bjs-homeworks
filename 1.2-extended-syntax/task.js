3d1572c82eea4567cf4b228d0f4bdd2d69d67967
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-04-12 15:24:54 +0300

    Update task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,8 +1,26 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
-}
-
+let quadraticEquation = (a, b, c) => {
+    if(a == 0);
+    let res = { };
+    let D = b * b - 4 * a * c;
+    console.log('D = ' + D);
+    if(D < 0);
+    res['discriminant'] = D;
+    if(D == 0)
+        res["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+    else if (D > 0) {
+        let tmp = [];
+        tmp.push((-b + Math.sqrt(D)) / (2 * a));
+        tmp.push((-b - Math.sqrt(D)) / (2 * a));
+        res["quadratic roots"] = tmp;
+    }
+    return res;
+ }
+  console.log(quadraticEquation(2,4,-3));
+// ответ:D = 40
+{
+  discriminant: 40,
+  'quadratic roots': [ 0.5811388300841898, -2.58113883008419 ]
+    
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
     // return averageMark;
