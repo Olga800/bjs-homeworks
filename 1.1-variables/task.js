6e2e5e04c0ccc9ff26a64e9dbac09f4d7db8864c
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-04-18 16:58:16 +0300

    Update task.js

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -14,19 +14,11 @@
 
         alert( average( 1, 2, 3, 4, 5, 6, 7, 8, 9) );
 //ответ -5
+
+
    
-
 function sayHello(userName) {
-	
-let myName = 'Мария Степановна';
-let message = 'Привет, мир! Меня зовут' + myName';
-}
-return  Привет, мир! Меня зовут' + ${userName};
-}
 
-function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+  return `Привет, мир! Меня зовут + ${userName}`;
+
 }
