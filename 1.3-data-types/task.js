c7a6c83472dd1e5327044fbe9417b35a050b0db6
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-04-20 14:43:27 +0300

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,5 +1,27 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
+    <input type='text' id="pointspossible"/>
+<input type='text' id="pointsgiven" />
+<input type='text' id="pointsperc" disabled/>
+JavaScript :
+    $(function(){
+    $('#pointspossible').on('input', function() {
+      calculate();
+    });
+    $('#pointsgiven').on('input', function() {
+     calculate();
+    });
+    function calculate(){
+        var pPos = parseInt($('#pointspossible').val()); 
+        var pEarned = parseInt($('#pointsgiven').val());
+        var perc="";
+        if(isNaN(pPos) || isNaN(pEarned)){
+            perc=" ";
+           }else{
+           perc = ((pEarned/pPos) * 100).toFixed(3);
+           }
+        $('#pointsperc').val(perc);
+    }
+});
     // return totalAmount;
 }
 
