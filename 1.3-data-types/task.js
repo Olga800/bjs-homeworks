34c3efc98294258eddfd984c059fb763c996f289
Olga800 <72503619+Olga800@users.noreply.github.com>
2021-04-23 10:18:00 +0300

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -19,6 +19,14 @@ console.log(data);
     // return totalAmount;
 }
 
+let loanAmount = 5000000;
+let initialPayment = 120000;
+let s = loanAmount - initialPayment;
+let percent = 8;
+let P = percent/(12*100); 
+let n = 12;
+let calculation = s*(P+P/(((1+P)^n)-1)).toFixed(2);
+console.log(calculation);
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     // return greeting;
