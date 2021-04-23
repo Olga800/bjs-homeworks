fe54d879383d2272560eace730d294e821833596
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-04-23 10:12:04 +0300

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,29 +1,13 @@
 
- <input type='text' id="pointspossible"/>
-<input type='text' id="pointsgiven" />
-<input type='text' id="pointsperc" disabled/>
+let loanAmount = 5000000;
+let initialPayment = 120000;
+let s = loanAmount - initialPayment;
+let percent = 8;
+let P = percent/(12*100); 
+let n = 12;
+let calculation = s*(P+P/(((1+P)^n)-1));
+console.log(calculation);
 
-     $(function(){
-    $('#pointspossible').on('input', function() {
-      calculate();
-    });
-    $('#pointsgiven').on('input', function() {
-     calculate();
-    });
-    function calculate(){
-        var pPos = parseInt($('#pointspossible').val()); 
-        var pEarned = parseInt($('#pointsgiven').val());
-        var perc="";
-        if(isNaN(pPos) || isNaN(pEarned)){
-            perc=" ";
-           }else{
-           perc = ((pEarned/pPos) * 100).toFixed(3);
-           }
-
-        $('#pointsperc').val(perc);
-    }
-
-});
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
