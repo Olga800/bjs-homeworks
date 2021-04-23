c187a047f04f65d48baef9b251e565fe2dd99152
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-04-23 13:37:55 +0300

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,13 +1,26 @@
 
-let loanAmount = 5000000;
-let initialPayment = 120000;
-let s = loanAmount - initialPayment;
-let percent = 8;
-let P = percent/(12*100); 
+let amount = 5000000;
+let contribution = 120000;
+let s = amount - contribution;
+let percent = 25;
+let p = percent/(12*100); 
 let n = 12;
-let calculation = s*(P+P/(((1+P)^n)-1));
+let date = 60;
+
+let calculation = s * (p + p/ (((1+p) ^ n)-1)).toFixed(2);
+
 console.log(calculation);
 
+let  totalAmount = calculation * date;
+
+console.log(totalAmount);
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+   
+    return totalAmount;
+}
+  
+Задача № 2
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
