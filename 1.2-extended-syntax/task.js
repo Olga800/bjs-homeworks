a0dcce0acaf08279c8c733ad6dfa046d33405ff5
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-05-06 08:33:09 +0300

    Update task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -5,14 +5,14 @@ let quadraticEquation = (a, b, c)  {
     let D = b * b - 4 * a * c;
     console.log('D = ' + D);
     }
-    if(D < 0){
+    if(D < 0) {
         return false;
     res['discriminant'] = D;
        }
     if(D == 0) {
         res["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
         }
-    else if(D > 0){
+    else if(D > 0) {
         let tmp = [];
         tmp.push((-b + Math.sqrt(D)) / (2 * a));
         tmp.push((-b - Math.sqrt(D)) / (2 * a));
