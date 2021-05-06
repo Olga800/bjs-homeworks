74bb3b553deab883c2339c5440200919be59edba
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-05-06 08:30:06 +0300

    Update task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,17 @@
 let quadraticEquation = (a, b, c) => {
-    if(a == 0)
+    if(a == 0) {
         return false;
     let res = {};
     let D = b * b - 4 * a * c;
     console.log('D = ' + D);
-    if(D < 0)
+    }
+    if(D < 0){
         return false;
     res['discriminant'] = D;
-    if(D == 0)
+       }
+    if(D == 0) {
         res["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+        }
     else if(D > 0){
         let tmp = [];
         tmp.push((-b + Math.sqrt(D)) / (2 * a));
@@ -18,6 +21,7 @@ let quadraticEquation = (a, b, c) => {
     return res;
 }
 console.log(quadraticEquation(1,12,36))
+}
     
 function getAverageMark(marks) {
   
