93ffcfb97278e8c36dbd5e4d462079f746bdde1a
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-05-08 07:13:09 +0300

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,29 +1,29 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
- 
-let amount = а;
-let contribution = b;
+let amount = 500000;
+let contribution = 90000;
 let s = amount - contribution;
-let percent = c;
+let percent = 45;
 let p = percent/(12*100);
-let date = d;
-let n = t;
+let date = 60;
+let n = 12;
 let monthlyPayment = s*(p + p / (((1+p) ** n)-1)).toPrecision(2);
- 
-console.log(monthlyPayment);
- 
+
+console.log(monthlyPayment.percent());
+
 let totalAmount = monthlyPayment * date; 
- 
-console.log(totalAmount); 
+
+console.log(totalAmount);
  
     return totalAmount;
- } 
-}
+ }
+  
+
   
  function getGreeting() {
     let name = prompt("Введите имя");
     if (name != "null") {
     console.log('Привет, мир! Меня зовут' + ' ' + name);
-}   else (!name || name == "null") {
+}   else  {
     console.log('Привет, мир! Меня зовут' + ' ' + 'Аноним');
 };
     return greeting;
@@ -32,4 +32,4 @@ console.log(totalAmount);
      console.log(getGreeting ('')); 
      console.log(getGreeting ());
      console.log(getGreeting (null)); 
-}
+
