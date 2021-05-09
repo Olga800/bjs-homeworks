0b2dea3e362a4b1725796d420099da7b4fcdc726
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-05-09 17:11:09 +0300

    Update task.js

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,33 +1,15 @@
-function getSolutions(a, b, c) {
-  let d = b * b - 4 * a * c;
-  if (d < 0) {
-    string = `пара комплексно-сопряженных корней<br>x<sub>1</sub> = (`;
-    string += - b / (2 * a);
-    string += Math.sqrt(- d) / (2 * a);
-    string += ` ), x<sub>2</sub> = (`;
-    string += - b / (2 * a);
-    string += `,`;
-    string += - Math.sqrt(-d) / (2 * a);
-    return d: `значение_дискриминанта`, roots: [];
-  } else {
-    if (d == 0) {
-      let x1 = -b / 2a;
-      string = `два одинаковых вещественных корня:<br>x<sub>1</sub> = x<sub>2</sub> = `;
-      string += -b / (2 * a);
-      return d:, roots: [значение_x1];
-    } else (d > 0){
-      let x1 = (-b + √D) / 2a;
-      let x2 = (-b - √D) / 2a; 
-      string = `два различных вещественных корня:<br>x<sub>1</sub> = `;
-      string += -b / (2 * a) - Math.sqrt(d) / (2 * a);
-      string += ", x<sub>2</sub> = ";
-      string += -b / (2 * a) + Math.sqrt(d) / (2 * a);
-    }
-  }
-  getSolutions(d:, roots: [значение_x1, значение_x2]);
-
+function getResult(a, b, c) {
+  const x = [];
+  const discr = Number(b ** 2 - 4 * a * c);
+  if (discr > 0) {
+    x[0] = Number(((-1 * b) + Math.sqrt(discr)) / (2 * a));
+    x[1] = Number(((-1 * b) - Math.sqrt(discr)) / (2 * a));
+  } else if (discr === 0) {
+    x[0] = -1 * b / (2 * a);
+  };
+  let x = showSolutionsMessage;
+}
   function showSolutionsMessage(a, b, c) {
-    let result = showSolutionsMessage(a, b, c);
     concole.log(`Вычисляем корни квадратного уравнения ax² + bx + c»`);
     concole.log(`Значение дискриминанта: D`);
     if (d < 0) {
