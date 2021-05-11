fc548c74daaf309fa7b67034fd4485803e90fc6a
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-05-11 20:54:42 +0300

    Update task.js

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -10,3 +10,41 @@ function showSolutionsMessage(a, b, c) {
     console.log('Уравнение не имеет вещественных корней');
   }
 }
+
+function getAverageScore(data) {
+    let averageScore = {};
+    let objectLength = 0;
+
+    for (let key in data) {
+        averageScore[key] = getAverageMark(data[key]);
+        objectLength++;
+    }
+    if (objectLength === 0) {
+        return averageScore.average = 0;
+    }
+
+    averageScore.average = (Object.values(averageScore).reduce((a, b) => a + b)) / objectLength;
+
+    return averageScore;
+}
+
+function getAverageMark(marks) {
+    let sum = 0;
+
+    if (marks.length === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+
+    let averageSum = sum / marks.length;
+    
+    return averageSum;
+}
+
+console.log(getAverageScore({geometry: [2, 4, 5],
+    algebra: [2,4,5,2,3,4],
+    russian: [3,3,4,5],
+    physics:[5,5]}));
