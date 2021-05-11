26e5878105c34a0abdd8735f446325f50e0451d6
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-05-11 20:47:09 +0300

    Update task.js

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,28 +1,12 @@
-function getResult(a, b, c) {
-  const x = [];
-  const discr = Number(b ** 2 - 4 * a * c);
-  if (discr > 0) {
-    x[0] = Number(((-1 * b) + Math.sqrt(discr)) / (2 * a));
-    x[1] = Number(((-1 * b) - Math.sqrt(discr)) / (2 * a));
-  } else if (discr === 0) {
-    x[0] = -1 * b / (2 * a);
-  };
-};
-  function showSolutionsMessage(a, b, c) {
-    concole.log(`Вычисляем корни квадратного уравнения ax² + bx + c`);
-    concole.log(`Значение дискриминанта: discr`);
-    if (discr < 0) {
-      concole.log(`Значение дискриминанта: discr`);
-      concole.log(`Уравнение не имеет вещественных корней`);
-    };
-    if (discr == 0) {
-      concole.log(`Значение дискриминанта: discr`);
-      concole.log(`Уравнение имеет один корень X₁ = значение_корня`);
-    };
-    if (discr == 0) {
-      concole.log(`Значение дискриминанта: discr`);
-      concole.log(`Уравнение имеет два корня. X₁ = значение_корня_1, X₂ = значение_корня_2`);
-    };
+function showSolutionsMessage(a, b, c) {
+  const result = getSolutions(a, b, c);
+  console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+  console.log(`Значение дискриминанта: ${result.D}`);
+  if (result.D > 0) {
+    console.log(`Уравнение имеет два корня X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+  } else if (result.D === 0) {
+    console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+  } else if (result.D < 0) {
+    console.log('Уравнение не имеет вещественных корней');
   }
-  showSolutionsMessage();
-
+}
