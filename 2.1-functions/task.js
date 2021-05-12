5486c2225f6161d831c8c0c7062ea2405561a2f0
Olenka35 <82054039+Olenka35@users.noreply.github.com>
2021-05-12 20:17:38 +0300

    Update task.js

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,12 +1,21 @@
 function showSolutionsMessage(a, b, c) {
+  
   const result = getSolutions(a, b, c);
+  
   console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+  
   console.log(`Значение дискриминанта: ${result.D}`);
+  
   if (result.D > 0) {
+    
     console.log(`Уравнение имеет два корня X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+    
   } else if (result.D === 0) {
+    
     console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+    
   } else if (result.D < 0) {
+    
     console.log('Уравнение не имеет вещественных корней');
   }
 }
